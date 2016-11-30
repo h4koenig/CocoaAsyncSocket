648f9e0a571fe04bea78249f66992b5c3ef0be0e
Heinrich Koenig <hkoenig@sputs.com>
2016-11-29 21:28:21 -0700

    Add watchOS target

---
+++ watchOS CocoaAsyncSocket/watchOS CocoaAsyncSocket.h
@@ -0,0 +1,23 @@
+//
+//  watchOS CocoaAsyncSocket.h
+//  watchOS CocoaAsyncSocket
+//
+//  Created by Heinrich Koenig on 11/29/16.
+//  Copyright © 2016 Robbie Hanson. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for watchOS CocoaAsyncSocket.
+FOUNDATION_EXPORT double watchOS_CocoaAsyncSocketVersionNumber;
+
+//! Project version string for watchOS CocoaAsyncSocket.
+FOUNDATION_EXPORT const unsigned char watchOS_CocoaAsyncSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <watchOS_CocoaAsyncSocket/PublicHeader.h>
+
+#import <CocoaAsyncSocket/AsyncSocket.h>
+#import <CocoaAsyncSocket/AsyncUdpSocket.h>
+#import <CocoaAsyncSocket/GCDAsyncSocket.h>
+#import <CocoaAsyncSocket/GCDAsyncUdpSocket.h>
+
